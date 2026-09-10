6a125eb993b8c10c42ca2fc2bca55736a9920979
Vishnupriya R <vpvpvp6092@gmail.com>
2026-09-10 21:55:00 +0530

    Create character_count.js

---
+++ character_count.js
@@ -0,0 +1,10 @@
+//check how many times a character occur in a string
+let str="priyaa";
+let char="a";
+let count=0;
+for(let i=0;i<str.length;i++){
+  if(str[i]==char){
+      count++;
+  }
+}
+console.log(count);
