f907de57ad8bbf27e07e65f0420c30e74cc203b3
Vishnupriya R <vpvpvp6092@gmail.com>
2026-09-19 15:07:15 +0530

    Create Remove_adjacent_duplicates.js

---
+++ Remove_adjacent_duplicates.js
@@ -0,0 +1,11 @@
+let s = "abbaca";
+let stack = [];
+
+for (let i = 0; i < s.length; i++) {
+    if (stack.length > 0 && stack[stack.length - 1] === s[i]) {
+        stack.pop();
+    } else {
+        stack.push(s[i]);
+    }
+}
+console.log(stack.join(""));
